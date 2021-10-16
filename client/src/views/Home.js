62b8c428b50ee7bef2eb01aa1c373b9f5c61894d
Manveer-Saini <zolbly@gmail.com>
2021-10-15 22:06:44 -0700

    Views and Components added

---
+++ client/src/views/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Router } from "@reach/router";
+import { Link, navigate } from '@reach/router';
+import NavBar from '../components/NavBar.js';
+import men from "../images/men.png";
+import women from "../images/women.jpeg";
+
+
+
+const Home = () => {
+
+    return (
+        <div style={{ textAlign: "center" }}>
+            <NavBar />
+            <br />
+            <div style={{ width: "60%", height: "100%", textAlign: "space-between", display: "flex", flexDirection: "row", margin: "auto" }}>
+                <div style={{ display: "flex", flexDirection: "column", textAlign: "center", margin: "30px" }}>
+                    <img src={men} alt="men"></img>
+                    <br />
+                    <button type="button" className="btn btn-outline-success"><Link to={'/'}>Mens Clothing </Link></button>
+                </div>
+                <div style={{ display: "flex", flexDirection: "column", textAlign: "center", margin: "30px" }}>
+                    <img src={women} alt="women"></img>
+                    <br />
+                    <button type="button" className="btn btn-outline-warning"><Link to={'/'}>Womens Clothing </Link></button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
