43d43a670001516ff6a3c25403b7872a1c8c10b9
Fumie <funismo@gmail.com>
2021-10-17 13:14:15 -0700

    added styling on UserProfile

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { navigate } from '@reach/router';
 import axios from 'axios';
-import Form from './Form';
 import { Link } from '@reach/router';
+import Header from "./Header";
 
 const UserProfile = (props) => {
     const [userAlbumList, setUserAlbumList] = useState([]);
@@ -32,23 +32,43 @@ const UserProfile = (props) => {
     },[])
 
     return (
-<div style={{textAlign:"center"}}>
+        <div>
+            <Header/>
             <h1>Welcome to {userPage.username}'s Profile!</h1>
+            <div style={{textAlign:"center", width: "90%", margin:"auto" ,display: "flex", flexDirection: "row"}}>
+                <div style={{textAlign:"center", width: "40%", margin:"auto"}}>
+                    <h3> User Proifile</h3>
+                    <table className="table table-stripe">
+                        <tbody>
+                        <tr><td>First Name:</td> <td>{userPage.firstName} </td></tr>
+                        <tr><td>Last Name:</td> <td>{userPage.lastName}</td></tr>
+                        <tr><td>Username:</td> <td>{userPage.username} F</td></tr>
+                        </tbody>
+                        </table>
 
-            {
-                userAlbumList.map((album, index)=>(
-                    <div key={index}>
-                        <p>{album.name}</p>
-                        <img src={album.image} alt="image" />
-                        <p>{album.era}</p>
-                        <p>{album.genre}</p>
-                        <p>{album.rating}</p>
-                        <Link to="/home">Home</Link>
+
+                </div>
+                <div style={{textAlign:"center", width: "60%", margin:"auto"}}>
+                    <h3>Your favorite albums: </h3>
+                    <div style={{textAlign:"center", width: "70%", margin:"auto" ,display: "flex", flexDirection: "row", justifyContent: "flex start"}}>
+                    {
+                        userAlbumList.map((album, index)=>(
+                            <div key={index} style={{
+                                margin:"30px",
+                                width:"30%",
+                                height:"30%",}}>
+                                <p>Album Name: {album.name}</p>
+                                <img style={{ width:"29%", height:"29%"}} src={album.image} alt="image" />
+                                <p>Album Era:{album.era}</p>
+                                <p>Album Genre: {album.genre}</p>
+                                <p>Your Rating: {album.rating}</p>
+                            </div>
+                        
+                        ))
+                    }
                     </div>
-                
-                ))
-            }
-
+                </div>
+            </div>
         </div>
     )
 
